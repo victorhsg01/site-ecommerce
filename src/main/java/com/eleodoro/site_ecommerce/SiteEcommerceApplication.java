493f27394d9b8d570aa58b5e3ec7bd68ab4b93e4
victorhsg01 <victor.schroeder.goncalves@escola.pr.gov.br>
2024-07-30 11:38:07 -0300

    site2

---
+++ src/main/java/com/eleodoro/site_ecommerce/SiteEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.eleodoro.site_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleodoro/site_ecommerce/SiteEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleodoro.site_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
